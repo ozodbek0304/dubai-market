8a34cda304bb5ffbe15151657262067f9a40902c
Ozodbek Abdisamatov <abdisamatovozodbek003@gmail.com>
2025-03-05 11:06:29 +0500

    server products

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
+import { queryClient } from "@/lib/querClient";
 import "@/styles/globals.css";
+import { QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
