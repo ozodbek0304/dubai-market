918d938a647699e1fef55c9c64bf280d8b00d0ff
Ozodbek Abdisamatov <abdisamatovozodbek003@gmail.com>
2025-03-08 13:08:33 +0500

    google anlytic

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,21 @@ export default function Document() {
   return (
     <Html lang="uz">
       <Head>
+
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-FQSHC6M39G"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-FQSHC6M39G', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
